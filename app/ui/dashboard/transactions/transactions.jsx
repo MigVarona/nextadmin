b5ef0ff96fe791efb06cbf4ebcaab43ca168d6cd
MigVarona <migvaronag@gmail.com>
2024-02-29 11:23:51 +0100

    transacionts

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import styles from "./transactions.module.css"
 
 const Transactions = () => {
@@ -5,7 +6,27 @@ const Transactions = () => {
     return (
         <div className={styles.container}>
             <h2 className={styles.title}>Latest Transactions</h2>
-            <table className={styles.table}></table>
+            <table className={styles.table}>
+            <thead>
+                <tr>
+                <td>Name</td>
+                <td>Status</td>
+                <td>Date</td>
+                <td>Amount</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <Image src="/noavatar.png" alt="" widht={40} height={40} className={styles.userImage} />
+
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+            </table>
         </div>
     )
 }
