bd85d10c1f789f400cff2855a9fa494eea2be1ef
MigVarona <migvaronag@gmail.com>
2024-02-29 14:38:43 +0100

    users page

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,6 +1,18 @@
+import Search from "@/app/ui/dashboard/search/search"
+import styles from "@/app/ui/dashboard/users/users.module.css"
+import Link from "next/link"
+
 const UsersPage = () => {
     return (
-        <div>UsersPage</div>
+        <div className={styles.container}>
+            <div className={styles.top}>
+            <Search placeholder="Search for a user..." />
+            <Link href="/dashboard/users/add">
+                <button className={styles.addButton}>Add</button>
+            </Link>
+            </div>
+            <table className={styles.table}></table>
+        </div>
 
     )
 }
