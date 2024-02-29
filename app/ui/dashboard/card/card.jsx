4dae10d6da766718d94171ddd788b4ad24b27f9d
MigVarona <migvaronag@gmail.com>
2024-02-29 11:15:04 +0100

    card & transactuions

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,10 +1,21 @@
-import styles from "./card.module.css"
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./card.module.css";
 
 const Card = () => {
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>987</span>
+        <span className={styles.detail}>
+        <span className={styles.positive}> 20% </span>less than previous month</span>
 
-    return (
-        <div className={styles.container}>Card</div>
-    )
-}
+        
+        
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -3,7 +3,10 @@ import styles from "./transactions.module.css"
 const Transactions = () => {
 
     return (
-        <div className={styles.container}>transactions</div>
+        <div className={styles.container}>
+            <h2 className={styles.title}>Latest Transactions</h2>
+            <table className={styles.table}></table>
+        </div>
     )
 }
 
