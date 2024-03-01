ee72fba8518d647364aff30436ee0d0998089f61
MigVarona <migvaronag@gmail.com>
2024-03-01 08:23:11 +0100

    pr

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,9 +1,26 @@
-
+"use client"
 
 import { MdSearch } from "react-icons/md"
 import styles from "./search.module.css"
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+
 
 const Search = ({placeholder}) => {
+  const searchParams = useSearchParams();
+  const {replace} = useRouter();
+  const pathname = usePathname();
+
+  const params = new URLSearchParams(searchParams)
+
+  params.set("test", "value")
+
+  replace(`${pathname}?${params}`)
+
+  console.log(searchParams)
+  console.log(pathname)
+
+
+
     return (
         <div className={styles.container}>
             <MdSearch/>
